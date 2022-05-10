c2457b89c918d096cd0a9db3aef199dcc079fa3f
jawale204 <jawale204@gmail.com>
2022-05-10 16:51:09 +0530

    .env file added

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("server....");
+const express = require("express");
+const app = express();
+const dotnet = require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`listning to port ${PORT}`);
+});
+
+app.get("/api/user", (req, res) => {
+  return res.status(201).json({ message: "hello world" });
+});
