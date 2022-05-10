aa61c2c5e02a5948964e17e6adec617e1c2084ff
jawale204 <jawale204@gmail.com>
2022-05-10 17:20:42 +0530

    router added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const dotnet = require("dotenv").config();
+const userRoutes = require("./routes/userRoutes");
 
 const PORT = process.env.PORT || 5000;
 
@@ -8,6 +9,8 @@ app.listen(PORT, () => {
   console.log(`listning to port ${PORT}`);
 });
 
-app.get("/api/user", (req, res) => {
-  return res.status(201).json({ message: "hello world" });
-});
+app.use("/api/user", userRoutes);
+
+// app.get("/api/user", (req, res) => {
+//   return res.status(201).json({ message: "hello world" });
+// });
