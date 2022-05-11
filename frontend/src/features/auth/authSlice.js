dc1edf992202da1e3378159c626c0760d1be455d
jawale204 <jawale204@gmail.com>
2022-05-11 23:55:56 +0530

    auth slic created

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  isLoading: false,
+  isError: false,
+  isSuccess: false,
+  message: "",
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {},
+});
+
+export default authSlice.reducer;
