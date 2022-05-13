b66cfd88b685465ce88ff1f3b5c8f135d9ba44c1
jawale204 <jawale204@gmail.com>
2022-05-13 12:05:02 +0530

    updateticket validator created

--- backend/validation/Ticket.js
+++ backend/validation/Ticket.js
@@ -15,3 +15,27 @@ exports.createTicketValidation = [
     .isLength({ min: 10, max: 500 })
     .withMessage("length between 10-500"),
 ];
+
+exports.updateTicketValidation = [
+  body("product")
+    .not()
+    .isEmpty()
+    .bail()
+    .withMessage("Product should not be empty")
+    .matches({ options: ["iphone", "Macbook Pro", "iMac", "iPad"] })
+    .withMessage("please select listed products"),
+  body("description")
+    .not()
+    .isEmpty()
+    .bail()
+    .withMessage("description should not be empty")
+    .isLength({ min: 10, max: 500 })
+    .withMessage("length between 10-500"),
+  body("status")
+    .not()
+    .isEmpty()
+    .bail()
+    .withMessage("status should not be empty")
+    .matches({ options: ["iphone", "Macbook Pro", "iMac", "iPad"] })
+    .withMessage("status not valid"),
+];
