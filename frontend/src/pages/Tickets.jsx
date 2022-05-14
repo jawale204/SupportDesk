5951f02480b303358f60f82e3cd36a7c4ffb4f6c
jawale204 <jawale204@gmail.com>
2022-05-14 18:14:12 +0530

    ticketItem added

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { getAllTickets, reset } from "../features/ticket/ticketSlice";
 import { toast } from "react-toastify";
+import BackButton from "../components/BackButton";
+import TicketItem from "../components/TicketItem";
 function Tickets() {
   const { tickets, isError, isSuccess, message, isLoading } = useSelector(
     (state) => state.ticket,
@@ -24,7 +26,23 @@ function Tickets() {
   if (isLoading) {
     return <div>spinning</div>;
   }
-  return <div>Tickets</div>;
+  return (
+    <>
+      <BackButton url="/" />
+      <h1>Tickets</h1>
+      <div className="tickets">
+        <div className="ticket-headings">
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+      </div>
+      {tickets.map((ticket) => {
+        return <TicketItem key={ticket._id} ticket={ticket} />;
+      })}
+    </>
+  );
 }
 
 export default Tickets;
