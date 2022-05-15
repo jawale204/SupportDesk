5c4442a83fbfa943463192e0533506a761ed3b4a
jawale204 <jawale204@gmail.com>
2022-05-15 18:44:38 +0530

    path imported

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const dotnet = require("dotenv").config();
 const userRoutes = require("./routes/userRoutes");
 const ticketRoutes = require("./routes/ticketRoutes");
