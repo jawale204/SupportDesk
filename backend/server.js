d0873a6fd066633feeca0ab9ee5eff38ea750573
jawale204 <jawale204@gmail.com>
2022-05-15 23:11:36 +0530

    path changes

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
 
   // FIX: below code fixes app crashing on refresh in deployment
   app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build/public/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
   });
 } else {
   app.get("/", (req, res) => {
